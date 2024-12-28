6dd2dc0e4e6cd4951cbbba301ff538efe629227d
Arad Soutehkeshan <arad.skn84@gmail.com>
2024-12-28 02:23:40 +0000

    Print results

--- sensors/light_sensor.py
+++ sensors/light_sensor.py
@@ -9,6 +9,7 @@ class LightSensor:
         try:
             data = self.i2c_bus.read_i2c_block_data(self.address, 0x10, 2)
             light_level = (data[0] << 8) | data[1]
+            print("Light Level: ", light_level, " lux")
             return light_level
         except Exception as e:
             print("BH1750 error:", e)
